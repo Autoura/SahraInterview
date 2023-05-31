3c62191bbb1dde7fb2a695d30c5ad63606071a14
Alex Bainbridge <alex.bainbridge@tstc.io>
2023-05-31 12:53:15 +0100

    Rename stream > interview

--- src/main/kotlin/furhatos/app/sahra/main.kt
+++ src/main/kotlin/furhatos/app/sahra/main.kt
@@ -4,7 +4,7 @@ import furhatos.app.sahra.flow.NoGUI
 import furhatos.skills.Skill
 import furhatos.flow.kotlin.*
 
-class StreamSkill : Skill() {
+class InterviewSkill : Skill() {
     override fun start() {
         Flow().run(NoGUI)
     }
