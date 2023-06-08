985ca70de864451c3dd8259e514064dd43427ff4
Alex Bainbridge <alex.bainbridge@tstc.io>
2023-06-08 09:17:02 +0100

    Show colours so clear when thinking / listening

--- src/main/kotlin/furhatos/app/sahra/flow/parent.kt
+++ src/main/kotlin/furhatos/app/sahra/flow/parent.kt
@@ -1,7 +1,32 @@
 package furhatos.app.sahra.flow
 
+import furhatos.app.sahra.LISTENING_ENDED
+import furhatos.app.sahra.LISTENING_STARTED
+import furhatos.app.sahra.THINKING_ENDED
+import furhatos.app.sahra.THINKING_STARTED
 import furhatos.flow.kotlin.*
+import java.awt.Color
 
 val Parent: State = state {
+
+    // --- Listening (So you know you can speak)
+
+    onEvent(LISTENING_ENDED, instant = true) {
+        furhat.ledStrip.solid(Color(0, 0, 0)) // Off
+    }
+
+    onEvent(LISTENING_STARTED, instant = true) {
+        furhat.ledStrip.solid(Color.GREEN)
+    }
+
+    // --- Thinking (So you know not to speak)
+
+    onEvent(THINKING_ENDED, instant = true) {
+        furhat.ledStrip.solid(Color(0, 0, 0)) // Off
+    }
+
+    onEvent(THINKING_STARTED, instant = true) {
+        furhat.ledStrip.solid(Color.RED)
+    }
 
 }
